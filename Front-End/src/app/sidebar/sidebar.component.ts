cec4c7df0ed69ba40cc3fb8c0be46fcda5ffc588
lijiltangentia <94680634+lijiltangentia@users.noreply.github.com>
2022-06-01 18:38:25 +0530

    UI change

--- Front-End/src/app/sidebar/sidebar.component.ts
+++ Front-End/src/app/sidebar/sidebar.component.ts
@@ -14,7 +14,7 @@ export const ROUTES: RouteInfo[] = [
     // { path: 'maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
     // { path: 'notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
     // { path: 'user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
-    { path: 'table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
+    // { path: 'table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
     // { path: 'typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
     { path: 'timesheet',    title: 'Timesheet Management',  icon:'nc-watch-time', class: 'm-item' },
     { path: 'projects',    title: 'Projects',  icon:'nc-settings', class: 'm-item' },
