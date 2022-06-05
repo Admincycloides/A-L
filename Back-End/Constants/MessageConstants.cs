1cb9f96b58bb8e0394ac738db60b9282d57f1517
vibhavkamat <90922479+vibhavkamat@users.noreply.github.com>
2022-06-05 12:33:21 +0530

    Changes done for Submit Timesheet and structural change modification for Timesheet Management API

--- Back-End/Constants/MessageConstants.cs
+++ Back-End/Constants/MessageConstants.cs
@@ -10,8 +10,13 @@
         public static readonly string TimsheetModificationFailed = "TimeSheet records modification Failed";
         public static readonly string TimsheetListingSuccess = "TimeSheet records fetched successfully";
         public static readonly string TimsheetListingFailed = "TimeSheet records fetch failed";
+        public static readonly string TimesheetListingNoRecords = "Employee has no timesheet records during the selected week!!";
         public static readonly string GenerateOTPFailed = "OTP Generation failed";
         public static readonly string GenerateOTPSuccess = "OTP Generated successfully";
+        public static readonly string TimesheetSubmissionSuccess = "Timesheet Submitted successfully";
+        public static readonly string TimesheetSubmissionFailed = "Timesheet Submission Failed";
+        public static readonly string SupervisorListingSuccess = "Supervisor records fetched successfully";
+        public static readonly string SupervisorListingFailed = "Supervisor records fetch failed";
+
     }
-}
 }
---
+++ Back-End/Repository/Implementation/ProjectRepository.cs
@@ -0,0 +1,21 @@
+﻿using AnL.Models;
+using AnL.Repository.Abstraction;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnL.Repository.Implementation
+{
+    public class ProjectRepository : Repository<ProjectDetails>, IProject
+    {
+        private DbContext _context;
+        private readonly IUnitOfWork _UOW;
+
+        DbSet<ProjectDetails> dbSet;
+        public ProjectRepository(DbContext context, IUnitOfWork UOW) : base(context)
+        {
+            this._context = context;
+            this._UOW = UOW;
+            dbSet = context.Set<ProjectDetails>();
+        }
+
+    }
+}
