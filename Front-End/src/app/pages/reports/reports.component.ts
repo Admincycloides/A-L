121a24c13e64af7dc4fd04d0472900b4f37b28b2
Jishnu <jishnup@tangentia.com>
2022-06-08 14:00:29 +0530

    report initial changes

--- Front-End/src/app/pages/reports/reports.component.ts
+++ Front-End/src/app/pages/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-reports',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent implements OnInit {
+  dropdownSettings: IDropdownSettings = {};
+  projectList: any[];
+  employeeList: any[]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+    };
+    this.projectList = ['Project 1','Project 2','Project 2','Project 3','Project 4']
+  }
+  public onProjectEmployeeDeSelect(item:any,value:any){
+    console.log(item);
+
+  }
+  public onProjectEmployeeSelect(item:any,value:any){
+    console.log(item);
+  }
+  public onProjectEmployeeSelectAll(item:any,value:any){
+    console.log(item);
+  }
+  public onProjectEmployeeDeSelectAll(item:any,value:any){
+    console.log(item);
   }
 
 }
