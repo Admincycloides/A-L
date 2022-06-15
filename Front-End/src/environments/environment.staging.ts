017879ba2af6823b8c92434a296fbe4e95fecd42
Jishnu <jishnup@tangentia.com>
2022-06-15 18:55:25 +0530

    changes added

--- Front-End/src/environments/environment.staging.ts
+++ Front-End/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     env:'STAGING',
     apiConfig: {
-      api: 'https://103.79.223.61:54',
+      api: 'http://103.79.223.61:52',
       server : ''
     }
   };
